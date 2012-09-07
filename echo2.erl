6220d87310028f207247145dd7fcdd8d0aa25f22
Jason <znlswd@gmail.com>
2012-09-07 22:26:46 +0800

    fix echo2

--- echo2.erl
+++ echo2.erl
@@ -3,23 +3,24 @@
 go()->
 	Pid = spawn(echo2,loop,[1]),
 	Pid ! {self(),hello},
-	receive
-		{Pid,Msg1} -> 
-			io:format("begin to receive 1~n"),
-			io:format("~w~n",[Msg1])
-	end,
 	Pid ! {self(),hello},
 	receive
-		{Pid,Msg2} -> 
-			io:format("begin to receive 2~n"),
-			io:format("~w~n",[Msg2])
+		{Pid,2} -> 
+			io:format("begin to receive 2~n",[]),
+			io:format("~w~n",[2])
+	end,
+	receive
+		{Pid,1} -> 
+			io:format("begin to receive 1~n",[]),
+			io:format("~w~n",[1])
 	end,
 	Pid ! stop.
+
 loop(Counter) ->
 	receive
 		{From,_} ->
+			io:format("receive for the ~wst time.~n",[Counter]),
 			From ! {self(),Counter},
-			io:format("receive for the ~w time.~n",Counter),
 			loop(Counter+1);
 		stop ->
 			true
